9ad33c3ddb509f5f46bbefa5e326d6180efb8e74
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2025-08-11 22:39:32 -0300

    Remove unused parameter.

--- c++-boost-lmdb/src/api/Config.h
+++ c++-boost-lmdb/src/api/Config.h
@@ -21,8 +21,6 @@ namespace rinhaback::api
 	public:
 		static inline const auto ioWorkers = static_cast<unsigned>(std::stoul(readEnv("IO_WORKERS", "8")));
 		static inline const auto handlerWorkers = static_cast<unsigned>(std::stoul(readEnv("HANDLER_WORKERS", "8")));
-		static inline const auto processorWorkers =
-			static_cast<unsigned>(std::stoul(readEnv("PROCESSOR_WORKERS", "8")));
 		static inline const auto database = readEnv("DATABASE", "/data/database");
 		static inline const auto databaseSize = static_cast<unsigned>(std::stoul(readEnv("DATABASE_SIZE", "10485760")));
 		static inline const auto coordinator = readEnv("COORDINATOR", "false") == "true";
